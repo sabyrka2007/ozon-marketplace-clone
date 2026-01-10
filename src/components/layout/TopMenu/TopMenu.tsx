ae1ceb22d824aba6ef1016ce5d179caf9ca5bf98
Sabyrka <sabyrka2007@gmail.com>
2026-01-10 12:52:32 +0500

    feat: location mock

--- src/components/layout/TopMenu/TopMenu.tsx
+++ src/components/layout/TopMenu/TopMenu.tsx
@@ -1,6 +1,7 @@
 import { topMenu } from '@/components/layout/TopMenu/top-menu.data'
 import Link from 'next/link'
 import cn from 'clsx'
+import { allowedDisplayValues } from 'next/dist/compiled/@next/font/dist/constants'
 
 export const TopMenu = () => {
 
@@ -8,23 +9,32 @@ export const TopMenu = () => {
   // TODO: Language selector
 
   return (
-    <nav className="flex gap-5 items-center px-5 pb-5 bg-white">
-      {topMenu.map((item) => (
-        <Link
-          href={item.href}
-          key={item.title}
-          className={cn(
-            'flex gap-1 items-center transition-all hover:opacity-100 hover:text-primary opacity-50',
-            item.variant === 'fresh' && 'text-teal-600 opacity-100 font-medium',
-          )}
-        >
-          {item.icon && <item.icon
-            size={16}
-            className="mr-0.5"
-          />}
-          <span>{item.title}</span>
-        </Link>
-      ))}
-    </nav>
+    <div>
+      <nav className="flex gap-5 items-center px-5 pb-5 bg-white">
+        {topMenu.map((item) => (
+          <Link
+            href={item.href}
+            key={item.title}
+            className={cn(
+              'flex gap-1 items-center transition-all hover:opacity-100 hover:text-primary opacity-50',
+              item.variant === 'fresh' && 'text-teal-600 opacity-100 font-medium',
+            )}
+          >
+            {item.icon && <item.icon
+              size={16}
+              className="mr-0.5"
+            />}
+            <span>{item.title}</span>
+          </Link>
+        ))}
+      </nav>
+
+      <div className="font-medium">
+        Москва &qt;
+        <button className="text-primary font-semibold">
+          Укажите вдрес
+        </button>
+      </div>
+    </div>
   )
 }
