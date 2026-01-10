98928d2351afae289945b8defedcbb51092ba0dc
Sabyrka <sabyrka2007@gmail.com>
2026-01-10 17:36:43 +0500

    feat: import react

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
+import React from 'react'
 
 export default async function LocaleLayout({ children, params }: {
   children: React.ReactNode
